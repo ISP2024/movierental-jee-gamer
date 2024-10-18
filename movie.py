aefc7196a995da3fec19d8dacc4c6d4af928391b
Jiramate Kedmake <jiramate2548@gmail.com>
2024-10-18 11:12:55 +0700

    Make Movie immutable

--- movie.py
+++ movie.py
@@ -1,4 +1,5 @@
 import logging
+from collections import Collection
 
 
 class Movie:
@@ -6,12 +7,29 @@ class Movie:
     A movie available for rent.
     """
 
-    def __init__(self, title):
+    def __init__(self, title: str, year: int, genre: Collection[str]):
         # Initialize a new movie. 
-        self.title = title
-    
-    def get_title(self):
-        return self.title
-    
+        self.__title = title
+        self.__year = year
+        self.__genre = genre
+
+    @property
+    def title(self):
+        return self.__title
+
+    @property
+    def year(self):
+        return self.__year
+
+    @property
+    def genre(self):
+        return self.__genre
+
     def __str__(self):
-        return self.title
+        return f"{self.title} ({self.year})"
+
+    def is_genre(self, genre: str):
+        that_genre = genre.lower()
+        return that_genre in self.__genre
+
+
